1a9bc32da4012f7cf15f317bedc0d5e22f209462
sanson01 <marcossantiagocolombo@gmail.com>
2025-01-19 00:26:22 +0100

    codigo subido s skecth js

--- bici/sketch.js
+++ bici/sketch.js
@@ -1,64 +1,39 @@
-let flecha = [];
-let numFrames = 179; // Número total de imágenes
-let currentFrame = 0; // Frame actual
-let texts = [];
-let texts2 = [];
-
-let frameDuration = 33.333; // Duración de cada frame en milisegundos (1 segundo)
-let lastFrameTime = 0; // Variable para almacenar el tiempo del último frame
-let aumento = 1;
-//let progresion = 1.007;
+let images = [];
+let currentIndex = 0;
+let displayDuration = 100; // Duración de cada imagen en milisegundos (0.1 segundos por defecto)
+let lastSwitchTime = 0;
 
 function preload() {
-  // Cargar las imágenes
-  for (let i = 0; i < numFrames; i++) {
-    let filename = "frames/flecha_de_zenon_corto_3" + nf(i, 3) + ".jpg"; // nf(i, 3) asegura que el número tenga 3 dígitos, por ejemplo, 001 en lugar de 1
-    flecha[i] = loadImage(filename);
+  // Cargar las imágenes en un array
+  for (let i = 1; i <= 37; i++) {
+    let imageName = `madrid/madrid_${i}.jpeg`;
+    images.push(loadImage(imageName));
   }
 }
 
 function setup() {
-  createCanvas(720, 1280); // Ajusta el tamaño del lienzo según tus necesidades
-  frameRate(30); // Fija la tasa de frames a 1 por segundo
-  fill (255);
-  stroke (255);
+  createCanvas(1280, 870); // Tamaño del canvas según las dimensiones de las imágenes
+  frameRate(60);
 }
 
 function draw() {
-  //background(255);
-   
-  // Cambiar al siguiente frame después de frameDuration milisegundos
-  if (millis() - lastFrameTime >= frameDuration) {
-    currentFrame = (currentFrame + 1) % numFrames; // Avanzar al siguiente frame
-    //aumento = aumento * progresion;
-    if (currentFrame<=10)
-      aumento = aumento * 1;
-    else if (currentFrame<=20 && currentFrame>11)
-      aumento = aumento * 1.002;
-    else if (currentFrame<=30 && currentFrame>21)
-      aumento = aumento * 1.004;
-    else if (currentFrame<=40 && currentFrame>31)
-      aumento = aumento * 1.006;
-    //else if (currentFrame<=45 && currentFrame>41)
-      //aumento = aumento * 1.01;
-    else if (currentFrame>41)
-      aumento = aumento * 1.01;
-    
-    lastFrameTime = millis(); // Actualizar el tiempo del último frame
-    frameDuration = frameDuration * aumento;
-    console.log("Número de fotograma = " + currentFrame);
-    console.log("Duración de cada fotograma en segundos = " + frameDuration /1000);
-    
-    image(flecha[currentFrame], 0, 0); // Mostrar la imagen actual en (0, 0)
-  
-    for (let i = 0; i < texts.length; i++) {
-      text(texts[i], 30, height - 20 - (texts.length - 1 - i) * 30);
-    }
-    texts.push("Frame = " + String(currentFrame));
-    
-    for (let i = 0; i < texts2.length; i++) {
-      text(texts2[i], 30, (height - 20 - (texts2.length - 1 - i) * 30)-15);
-    }
-    texts2.push("Length = " + nf(frameDuration/1000, 0, 4) + " segs");
+  background(0); // Fondo negro
+
+  // Mostrar la imagen actual
+  image(images[currentIndex], 0, 0, width, height);
+
+  // Verificar si es hora de cambiar la imagen
+  if (millis() - lastSwitchTime > displayDuration) {
+    currentIndex = (currentIndex + 1) % images.length; // Pasar a la siguiente imagen en loop
+    lastSwitchTime = millis();
+  }
+}
+
+function keyPressed() {
+  // Cambiar la duración con las teclas de flecha hacia arriba y abajo
+  if (keyCode === UP_ARROW) {
+    displayDuration += 50; // Incrementa la duración en 50ms
+  } else if (keyCode === DOWN_ARROW) {
+    displayDuration = max(50, displayDuration - 50); // Disminuye la duración, mínimo 50ms
   }
 }
