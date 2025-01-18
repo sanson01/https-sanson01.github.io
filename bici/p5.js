d8e71152e137d0b17c26c3cdc81b49642a7b4f7e
sanson01 <marcossantiagocolombo@gmail.com>
2025-01-19 00:23:45 +0100

    primer prueba codigo

---
+++ bici/p5.js
@@ -0,0 +1,39 @@
+let images = [];
+let currentIndex = 0;
+let displayDuration = 100; // Duración de cada imagen en milisegundos (0.1 segundos por defecto)
+let lastSwitchTime = 0;
+
+function preload() {
+  // Cargar las imágenes en un array
+  for (let i = 1; i <= 37; i++) {
+    let imageName = `madrid/madrid_${i}.jpeg`;
+    images.push(loadImage(imageName));
+  }
+}
+
+function setup() {
+  createCanvas(1280, 870); // Tamaño del canvas según las dimensiones de las imágenes
+  frameRate(60);
+}
+
+function draw() {
+  background(0); // Fondo negro
+
+  // Mostrar la imagen actual
+  image(images[currentIndex], 0, 0, width, height);
+
+  // Verificar si es hora de cambiar la imagen
+  if (millis() - lastSwitchTime > displayDuration) {
+    currentIndex = (currentIndex + 1) % images.length; // Pasar a la siguiente imagen en loop
+    lastSwitchTime = millis();
+  }
+}
+
+function keyPressed() {
+  // Cambiar la duración con las teclas de flecha hacia arriba y abajo
+  if (keyCode === UP_ARROW) {
+    displayDuration += 50; // Incrementa la duración en 50ms
+  } else if (keyCode === DOWN_ARROW) {
+    displayDuration = max(50, displayDuration - 50); // Disminuye la duración, mínimo 50ms
+  }
+}
