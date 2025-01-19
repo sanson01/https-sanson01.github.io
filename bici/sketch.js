0590dce142de66bfffa1b58ea8d39a623e0a6076
sanson01 <marcossantiagocolombo@gmail.com>
2025-01-19 01:08:54 +0100

    contador

--- bici/sketch.js
+++ bici/sketch.js
@@ -13,6 +13,9 @@ function preload() {
 
 function setup() {
   createCanvas(1280, 870); // Tamaño del canvas según las dimensiones de las imágenes
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  fill(255); // Color del texto
 }
 
 function draw() {
@@ -20,6 +23,11 @@ function draw() {
 
   // Mostrar la imagen actual
   image(images[currentIndex], 0, 0, width, height);
+
+  // Mostrar el texto con el número de imagen y el contador
+  let elapsedTime = (millis() - lastSwitchTime) / 1000; // Tiempo transcurrido en segundos
+  let displayText = `image ${currentIndex + 1}/37; ${elapsedTime.toFixed(2)} secs`;
+  text(displayText, width / 2, height - 30); // Texto centrado en la parte inferior
 
   // Verificar si es hora de cambiar la imagen
   if (millis() - lastSwitchTime > displayDuration) {
