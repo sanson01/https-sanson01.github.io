1c8a64d1467312293e8e29e1b0ae8073ebb0c138
sanson01 <marcossantiagocolombo@gmail.com>
2025-01-19 01:03:40 +0100

    test 4

--- bici/sketch.js
+++ bici/sketch.js
@@ -1,6 +1,6 @@
 let images = [];
 let currentIndex = 0;
-let seconds_number = 50; // Duración en segundos
+let displayDuration = 100; // Duración en milisegundos (0.1 segundos por defecto)
 let lastSwitchTime = 0;
 
 function preload() {
@@ -13,8 +13,6 @@ function preload() {
 
 function setup() {
   createCanvas(1280, 870); // Tamaño del canvas según las dimensiones de las imágenes
-  frameRate(60);
-  lastSwitchTime = millis(); // Asegurarse de inicializar el tiempo al inicio
 }
 
 function draw() {
@@ -24,18 +22,8 @@ function draw() {
   image(images[currentIndex], 0, 0, width, height);
 
   // Verificar si es hora de cambiar la imagen
-  if (millis() - lastSwitchTime >= seconds_number * 1000) {
+  if (millis() - lastSwitchTime > displayDuration) {
     currentIndex = (currentIndex + 1) % images.length; // Pasar a la siguiente imagen en loop
-    lastSwitchTime = millis(); // Reiniciar el temporizador
+    lastSwitchTime = millis();
   }
 }
-
-/*function keyPressed() {
-  // Cambiar la duración con las teclas de flecha hacia arriba y abajo
-  if (keyCode === UP_ARROW) {
-    seconds_number += 0.1; // Incrementa la duración en 0.1 segundos
-  } else if (keyCode === DOWN_ARROW) {
-    seconds_number = max(0.1, seconds_number - 0.1); // Disminuye la duración, mínimo 0.1 segundos
-  }
-  console.log(`Duración actual: ${seconds_number.toFixed(1)} segundos`);
-}*/
